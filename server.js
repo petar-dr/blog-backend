719dd097cc1a477eb0c96a6264867753b2bf180d
petar-dr <petardragojlovicbb.com>
2024-09-10 19:29:28 +0200

    add mongoose

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://pera91bb:u15bD718Cl61ts1v@blogdata.ra7xm.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then((con) => {
+    console.log(con.connections);
+    console.log("Mungoose is conected!");
+  });
 
 const app = express();
 
